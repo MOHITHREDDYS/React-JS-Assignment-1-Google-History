31063e2ef351ce4b79c3de090e311f63ae8d0e74
MOHITHREDDYS <mohithreddys.531@gmail.com>
2022-10-13 18:02:23 +0000

    added all required coding

---
+++ src/components/BrowserHistory/index.js
@@ -0,0 +1,145 @@
+import {Component} from 'react'
+import './index.css'
+import HistoryItem from '../HistoryItem'
+
+const initialHistoryList = [
+  {
+    id: 0,
+    timeAccessed: '07:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
+    title: 'Instagram',
+    domainUrl: 'instagram.com',
+  },
+  {
+    id: 1,
+    timeAccessed: '05:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
+    title: 'Twitter. It’s what’s happening / Twitter',
+    domainUrl: 'twitter.com',
+  },
+  {
+    id: 2,
+    timeAccessed: '04:35 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
+    title: 'Facebook – log in or sign up',
+    domainUrl: 'facebook.com',
+  },
+  {
+    id: 3,
+    timeAccessed: '04:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
+    title: 'LinkedIn: Log In or Sign Up',
+    domainUrl: 'linkedin.com',
+  },
+  {
+    id: 4,
+    timeAccessed: '04:00 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
+    title: 'Hashnode: Everything you need to start blogging as a developer!',
+    domainUrl: 'hashnode.com',
+  },
+  {
+    id: 5,
+    timeAccessed: '03:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
+    title: 'GitHub: Where the world builds software · GitHub',
+    domainUrl: 'github.com',
+  },
+
+  {
+    id: 6,
+    timeAccessed: '02:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
+    title: 'React – A JavaScript library for building user interfaces',
+    domainUrl: 'reactjs.org',
+  },
+  {
+    id: 7,
+    timeAccessed: '01:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
+    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
+    domainUrl: 'stackoverflow.com',
+  },
+
+  {
+    id: 8,
+    timeAccessed: '09:25 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
+    title: 'Gmail',
+    domainUrl: 'mail.google.com',
+  },
+  {
+    id: 9,
+    timeAccessed: '09:00 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    title: 'Google',
+    domainUrl: 'google.com',
+  },
+]
+
+class BrowserHistory extends Component {
+  state = {historyList: initialHistoryList, searchInput: ''}
+
+  onChangingInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  deleteHistory = id => {
+    const {historyList} = this.state
+    const afterDeletedList = historyList.filter(history => history.id !== id)
+    this.setState({historyList: afterDeletedList})
+  }
+
+  render() {
+    const {historyList, searchInput} = this.state
+
+    const filteredList = historyList.filter(history =>
+      history.title.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div>
+        <div className="top_container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+            alt="app logo"
+            className="logo"
+          />
+          <div className="search_container">
+            <div>
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/search-img.png"
+                alt="search"
+                className="search"
+              />
+            </div>
+            <hr />
+            <input
+              type="search"
+              placeholder="Search History"
+              value={searchInput}
+              onChange={this.onChangingInput}
+            />
+          </div>
+        </div>
+        {filteredList.length === 0 ? (
+          <p className="history">There is no history to show</p>
+        ) : (
+          <div className="list_container">
+            <ul>
+              {filteredList.map(eachHistory => (
+                <HistoryItem
+                  deleteHistory={this.deleteHistory}
+                  key={eachHistory.id}
+                  history={eachHistory}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default BrowserHistory
---
+++ src/components/HistoryItem/index.js
@@ -0,0 +1,34 @@
+import './index.css'
+
+const HistoryItem = props => {
+  const {history, deleteHistory} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = history
+
+  const onClickingDeleteIcon = () => {
+    deleteHistory(id)
+  }
+
+  return (
+    <li>
+      <p className="time">{timeAccessed}</p>
+      <div className="app_delete_container">
+        <div className="logo_title_container">
+          <img src={logoUrl} alt="domain logo" className="app_logo" />
+          <div className="title_url_container">
+            <p className="title">{title}</p>
+            <p className="url">{domainUrl}</p>
+          </div>
+        </div>
+        <button type="button" onClick={onClickingDeleteIcon}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+            className="delete_icon"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default HistoryItem
